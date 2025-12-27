a8ab48ef3b2900319c69de55e7b59c53430f0f13
James <90067744+James-Bragg@users.noreply.github.com>
2025-12-27 19:43:51 +0000

    home page fix

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,8 @@ import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+  <BrowserRouter basename="/Portfolio-Site">
+    <App />
+  </BrowserRouter>
   </StrictMode>,
 );
